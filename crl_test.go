2d962967dbb93419700a2736ad1f3e10b930b804
Koen Rouwhorst <info@koenrouwhorst.nl>
2017-12-24 17:46:44 +0100

    Fixed method call.

--- crl_test.go
+++ crl_test.go
@@ -41,7 +41,7 @@ func TestFindCert(t *testing.T) {
 	s := new(big.Int)
 	s.SetString("17015245701990644280577643802745589798", 10)
 
-	test := FindCert(s, resp)
+	test := findCert(s, resp)
 
 	if test == nil {
 		t.Errorf("expected to find revoked certificate with serial number %q", s.String())
@@ -56,7 +56,7 @@ func TestFindNonExistingRevokedCert(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	test := FindCert(big.NewInt(0), resp)
+	test := findCert(big.NewInt(0), resp)
 
 	if test != nil {
 		t.Error("did not expect to find a revoked certificate")
