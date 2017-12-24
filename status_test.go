9973c7a4479558a4833529308ba320636b399350
Koen Rouwhorst <info@koenrouwhorst.nl>
2017-12-24 15:28:19 +0100

    Added test for status with revocation reason.

--- status_test.go
+++ status_test.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-func TestString(t *testing.T) {
+func TestStatusString(t *testing.T) {
 	s := new(big.Int)
 	s.SetString("17015245701990644280577643802745589798", 10)
 
@@ -22,6 +22,30 @@ func TestString(t *testing.T) {
 	expected := "Serial number: 17015245701990644280577643802745589798\n\n" +
 		"Status: Revoked\n" +
 		"Revoked at: 2017-12-20 23:59:59 +0000 UTC\n"
+
+	if got != expected {
+		t.Errorf("expected %q, got %q", expected, got)
+	}
+}
+
+func TestStatusWithReasonString(t *testing.T) {
+	s := new(big.Int)
+	s.SetString("17015245701990644280577643802745589799", 10)
+
+	tt := time.Date(2017, 12, 24, 23, 59, 59, 0, time.UTC)
+	st := &Status{
+		SerialNumber: s,
+		Status:       "Revoked",
+		Reason:       "Key compromise",
+		RevokedAt:    tt,
+	}
+
+	got := st.String()
+
+	expected := "Serial number: 17015245701990644280577643802745589799\n\n" +
+		"Status: Revoked\n" +
+		"Reason: Key compromise\n" +
+		"Revoked at: 2017-12-24 23:59:59 +0000 UTC\n"
 
 	if got != expected {
 		t.Errorf("expected %q, got %q", expected, got)
